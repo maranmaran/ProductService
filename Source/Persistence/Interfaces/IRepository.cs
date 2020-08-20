921798cc2c983df57422db7dcd91549ecd6493b4
Marko Urh <urh.marko@gmail.com>
2020-08-20 14:23:14 +0200

    [PROD-2] Product repository consuming source from acceptance criteria - unit tests

---
+++ Source/Persistence/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Gets all entities
+        /// </summary>
+        /// <param name="filter">Filter predicate</param>
+        /// <param name="cancellationToken"></param>
+        Task<IEnumerable<TEntity>> GetAll(
+            Expression<Func<TEntity, bool>> filter = null,
+            CancellationToken cancellationToken = default);
+    }
+}
